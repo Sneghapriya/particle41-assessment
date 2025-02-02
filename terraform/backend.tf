f41691c0fd907f63e7a308a9560056e8f6e3172b
Sneghapriya <sneghavijayan@gmail.com>
2025-02-02 17:51:20 +0530

    updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,5 @@ terraform {
  backend "gcs" {
     bucket = "terraform-state"
     prefix = "particle41/state" // Optional: Adjust the prefix as needed
-    credentials = "gcp_credentials.json"
   }
 }
